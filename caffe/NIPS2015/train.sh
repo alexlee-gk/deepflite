2557a0c577f24f76f24274a8023c7353ed30afc2
Tuomas Haarnoja <tuomas@rll3.(none)>
2015-05-19 20:50:53 -0700

    create caffe folder

---
+++ caffe/NIPS2015/train.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+#~/caffe-rnn/build/tools/caffe train --solver=solver.prototxt  # --snapshot=/media/data__/tuomas/find_target/models/model_cached_rnn_im_obs_to_state_iter_3500.solverstate 
+~/caffe-vgps/build/tools/caffe train --solver=solver.prototxt # --snapshot=/media/data__/tuomas/find_target/models/model_image_features_only_iter_2000.solverstate 
